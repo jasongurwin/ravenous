de4e04435a068e5c367078b8d285926e19354ac2
jasongurwin <>
2017-10-17 11:24:12 -0700

    Not Working but updates with final part of the project

--- src/App.js
+++ src/App.js
@@ -3,27 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 import BusinessList from './components/BusinessList/BusinessList.js';
 import SearchBar from './components/SearchBar/SearchBar.js';
+import Yelp from './util/Yelp.js';
 
-let business = {
-  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
-};
-
-const businesses = [business, business, business, business, business, business];
-
+// const businesses = [business, business, business, business, business, business];
 
 class App extends Component {
 
-  searchYelp(term,location,sortBy){
-    console.log('Searching Yelp with ' + term + ", " + location + ", " + sortBy)
-  };
+  constructor(){
+    super()
+    this.state = {businesses: []}
+    this.searchYelp.bind(this);
+  }
+
+    searchYelp(term,location,sortBy){
+       Yelp.search(term,location,sortBy).then(businesses => { this.setState(businesses:businesses) })
+    }
+
+
+
+  // searchYelp(term,location,sortBy){
+  //   Yelp.search(term,location,sortBy).then(businesses => { setState({businesses:businesses}) }
+  // };
 
 
 
---
+++ src/util/Yelp.js
@@ -0,0 +1,58 @@
+const clientId = 'YWTvYMczr02O-okP1kqj6g'
+const secret = '8ysH3vZ3Zjlqi86hRVg7lybqsRrtd9TE1GCuf23LM2ygEy5fZwKKiN3IMsCnBkks'
+let accessToken = ''
+let url = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/oauth2/token?grant_type=client_credentials&client_id=${clientId}&client_secret=${secret}`
+
+
+let Yelp = {
+
+  getAccessToken(){
+
+    if (accessToken){
+      return new Promise(resolve =>
+        resolve(accessToken));
+      }
+      return fetch(url, {method: 'POST'}).then(response=> { response.json() }).then(jsonResponse => {accessToken=jsonResponse.access_token});
+    },
+
+
+  search(){
+
+    return Yelp.getAccessToken().then(() =>
+    {
+      return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+        headers: {Authorization: `Bearer ${accessToken}`}
+      }).then(jsonResponse=>{
+        if (jsonResponse.businesses) {
+          return jsonResponse.businesses.map(business => (
+
+            {
+              id: business.id,
+              imageSrc: business.image_url,
+              name: business.name,
+              address: business.location.address1,
+              city: business.location.city,
+              state: business.location.state,
+              zipCode: business.location.zip_code,
+              category: business.categories.title,
+              rating: business.rating,
+              reviewCount: business.review_count
+
+            }
+
+
+
+          )
+        )}
+      });
+
+    })
+
+
+
+
+
+  }
+};
+
+export default Yelp
