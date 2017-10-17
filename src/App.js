6e7a78713912a0b5a744156313a062937c0dce0c
jasongurwin <>
2017-10-17 14:16:41 -0700

    All working

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
   }
 
     searchYelp(term,location,sortBy){
-       Yelp.search(term,location,sortBy).then(businesses => { console.log(businesses); this.setState(businesses:businesses) })
+       Yelp.search(term,location,sortBy).then(businesses => { this.setState({businesses:businesses}) })
     }
 
 
